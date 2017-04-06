fba8963aa5acdea575abc7420cc896928366f8b5
DDRDmakar <makarevich.98@mail.ru>
2017-04-06 05:08:33 +0300

    Comments and basic functions added.

--- src/main/java/tar/Tar.java
+++ src/main/java/tar/Tar.java
@@ -9,9 +9,16 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+// Основной класс, реализует "склеивание" файлов в один и их разделение.
 
 public final class Tar {
 
+    /**
+     * Function for easy getting file as one solid string.
+     * @param path - text file location
+     * @return one string - all content of given file
+     * @throws IOException if file reading failed
+     */
     private String getFullFile(String path) throws IOException {
             StringBuilder target = new StringBuilder();
             BufferedReader currentFile = new BufferedReader(new FileReader(path));
@@ -21,6 +28,12 @@ public final class Tar {
             return target.toString();
     }
 
+    /**
+     * Converts many text files into one, which stores
+     * their names and contents.
+     * @param filenames - array of file locations
+     * @param outname - name of new file
+     */
     void filesToStorage(String[] filenames, String outname) {
         StringBuilder target = new StringBuilder(); // Will contain all files and separators
 
@@ -40,11 +53,18 @@ public final class Tar {
 
     }
 
+    /**
+     * Converts one tar file into many text files.
+     * @param path - tar file location
+     */
     void storageToFiles(String path) {
+        // File contents and names storage
         List<String> splittedLines = new ArrayList<String>();
+        // Temporary buffer to get name/value of file char-by-char
         StringBuffer tempTarget = new StringBuffer();
-        boolean quote = false;
-        boolean escaped = false;
+
+        boolean quote = false; // If current character is in quotes
+        boolean escaped = false; // If current character is escaped
 
         try {
             String storageFileText = getFullFile(path);
@@ -69,8 +89,10 @@ public final class Tar {
 
         } catch (IOException e) { System.err.println("IO error: " + e.getMessage()); }
 
+        // Check, if we can have pairs name-text
         if(splittedLines.size() % 2 == 1) throw new IllegalTarFormatException("File contains incorrect parts \\\"name\\\"-\\\"text\\\"");
 
+        // Write files out
         for(int i = 0; i < splittedLines.size(); i += 2) {
             try { FileUtils.writeStringToFile(new File(splittedLines.get(i)), splittedLines.get(i+1)); }
             catch (IOException e) {
@@ -79,5 +101,20 @@ public final class Tar {
         }
     }
 
-    // TODO equals / hashcode / toString
+    //=================================================================
+
+    @Override
+    public boolean equals(Object other) {
+        if(other == this) return true;
+        if(other == null || !(other instanceof Tar)) return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "[Tar object]";
+    }
+
+    @Override
+    public int hashCode() { return 23; }
 }
